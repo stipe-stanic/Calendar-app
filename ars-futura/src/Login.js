10eaa8c369a84f7bfe128f9ade5e60adddb69740
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-15 18:25:19 +0100

    Login page, redirects to home page
    Used google's Oauth 2.0 to connect and give access to the user via google platform

---
+++ ars-futura/src/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import GoogleLogin from "react-google-login";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const responseGoogle = (response) => {
+    navigate("/home");
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <h2>Login page</h2>
+      <GoogleLogin
+        clientId="747061863296-f2hb1umn9dbp2lbmp27d5ac2mfmba3f3.apps.googleusercontent.com"
+        buttonText="Login with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy="single_host_origin"
+      />
+    </div>
+  );
+};
+
+export default Login;
