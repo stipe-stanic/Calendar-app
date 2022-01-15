495084dc9043e3f4aa115b3803f10f2b42258f9e
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-15 18:12:04 +0100

    Integration with the Google calendar API
    Able to post, get and delete events from the calendar API, currently only one test_user is verified in Google Cloud Platform. Needs permission first, before loging-in with different google e-mail

---
+++ ars-futura/src/APIContext.js
@@ -0,0 +1,181 @@
+import React, { useContext, createContext } from "react";
+
+const APIContext = createContext();
+
+const APIProvider = ({ children }) => {
+  var gapi = window.gapi;
+  var CLIENT_ID =
+    "747061863296-f2hb1umn9dbp2lbmp27d5ac2mfmba3f3.apps.googleusercontent.com";
+  var API_KEY = "AIzaSyAaNGo9QtR-7G8wNlY6mVv6yi-PSvVMb3s";
+  var DISCOVERY_DOCS = [
+    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  ];
+  var SCOPES = "https://www.googleapis.com/auth/calendar.events";
+
+  const addEvents = () => {
+    gapi.load("client:auth2", () => {
+      console.log("loaded client");
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      });
+
+      gapi.client.load("calendar", "v3", () => console.log("loaded calendar"));
+
+      gapi.auth2
+        .getAuthInstance()
+        .signIn()
+        .then(() => {
+          var event = {
+            summary: "Google I/O 2015",
+            location: "800 Howard St., San Francisco, CA 94103",
+            description:
+              "A chance to hear more about Google's developer products.",
+            start: {
+              dateTime: "2022-01-18T09:00:00-07:00",
+              timeZone: "America/Los_Angeles",
+            },
+            end: {
+              dateTime: "2022-01-18T17:00:00-07:00",
+              timeZone: "America/Los_Angeles",
+            },
+            recurrence: ["RRULE:FREQ=DAILY;COUNT=2"],
+            attendees: [
+              { email: "lpage@example.com" },
+              { email: "sbrin@example.com" },
+            ],
+            reminders: {
+              useDefault: false,
+              overrides: [
+                { method: "email", minutes: 24 * 60 },
+                { method: "popup", minutes: 10 },
+              ],
+            },
+          };
+
+          //POST the event
+          var request = gapi.client.calendar.events.insert({
+            calendarId: "primary",
+            resource: event,
+          });
+
+          request.execute((event) => {
+            window.open(event.htmlLink);
+          });
+        });
+    });
+  };
+
+  const getEvents = () => {
+    gapi.load("client:auth2", () => {
+      console.log("loaded client");
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      });
+
+      gapi.client.load("calendar", "v3", () => console.log("loaded calendar"));
+
+      gapi.auth2
+        .getAuthInstance()
+        .signIn()
+        .then(() => {
+          var request = gapi.client.calendar.events
+            .list({
+              calendarId: "primary",
+              timeMin: new Date().toISOString(),
+              showDeleted: false,
+              singleEvents: true,
+              maxResults: 10,
+              orderBy: "startTime",
+            })
+            .then((response) => {
+              const events = response.result.items;
+              console.log("EVENTS: ", events);
+              console.log(events[0].id);
+            });
+        });
+    });
+  };
+
+  const deleteEvents = () => {
+    gapi.load("client:auth2", () => {
+      console.log("loaded client");
+
+      gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      });
+
+      gapi.client.load("calendar", "v3", () => console.log("loaded calendar"));
+
+      gapi.auth2
+        .getAuthInstance()
+        .signIn()
+        .then(() => {
+          var request = gapi.client.calendar.events
+            .list({
+              calendarId: "primary",
+              timeMin: new Date().toISOString(),
+              showDeleted: false,
+              singleEvents: true,
+              maxResults: 10,
+              orderBy: "startTime",
+            })
+            .then((response) => {
+              const events = response.result.items;
+              console.log("EVENTS: ", events);
+              console.log(events[0].id);
+            });
+
+          let eventId = "4rsg1849gm2hocv70c6gqmu1a4_20220119T160000Z";
+
+          const deleteEvent = async (eventId) => {
+            try {
+              let response = await gapi.client.calendar.events.delete({
+                calendarId: "primary",
+                eventId: eventId,
+              });
+
+              if (response.data === "") {
+                return 1;
+              } else {
+                return 0;
+              }
+            } catch (error) {
+              console.log(`Error at deleteEvent --> ${error}`);
+              return 0;
+            }
+          };
+
+          deleteEvent(eventId)
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        });
+    });
+  };
+
+  return (
+    <APIContext.Provider value={{ addEvents, getEvents, deleteEvents }}>
+      {children}
+    </APIContext.Provider>
+  );
+};
+
+export const useAPIContext = () => {
+  return useContext(APIContext);
+};
+
+export { APIContext, APIProvider };
