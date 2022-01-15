4574a706563dc2b96a32e487ee681ce39efd18a7
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-15 18:13:44 +0100

    Wrapped App component with APIProvider component

--- ars-futura/src/index.js
+++ ars-futura/src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { APIProvider } from "./APIContext";
+// import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <APIProvider>
+      <App />
+    </APIProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
