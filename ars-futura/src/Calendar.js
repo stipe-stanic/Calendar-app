6181b1c921ff1b30e1fa7c03a0b42411b773af79
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-16 22:36:48 +0100

    Storing data from GET method in local storage
    Added compomenents

--- ars-futura/src/Calendar.js
+++ ars-futura/src/Calendar.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useAPIContext } from "./APIContext";
+import CreateEvent from "./CreateEvent";
+import EventList from "./EventList";
 
 const Calendar = () => {
-  const { addEvents, getEvents, deleteEvents } = useAPIContext();
+  const {
+    addEvents,
+    getEvents,
+    deleteEvents,
+    calendarEvents,
+    setCalendarEvents,
+  } = useAPIContext();
+
+  useEffect(() => {
+    if (localStorage.getItem("event")) {
+      let temp = JSON.parse(localStorage.getItem("event"));
+      setCalendarEvents(temp);
+    }
+  }, [setCalendarEvents]);
+
+  useEffect(() => {
+    localStorage.setItem("event", JSON.stringify(calendarEvents));
+  }, [calendarEvents]);
 
   return (
     <div>
       <h2>List</h2>
-
       <button onClick={addEvents}>Add Event</button>
       <button onClick={getEvents}>Get Events</button>
-
       <button onClick={deleteEvents}>Delete Event</button>
+      <EventList />
+      <CreateEvent />
     </div>
   );
 };
