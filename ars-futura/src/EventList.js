153b23a60513fa73c0db7d11bb616f98e88dd0cd
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-17 22:41:10 +0100

    Sorting events by days before rendering

--- ars-futura/src/EventList.js
+++ ars-futura/src/EventList.js
@@ -6,11 +6,14 @@ export default function EventList() {
   const { calendarEvents } = useAPIContext();
 
   return (
-    <section>
+    <section className="event-width">
       <h2>Events</h2>
       <hr />
       <div>
         {calendarEvents.map((item) => {
+          calendarEvents.sort((a, b) => {
+            return a.day - b.day;
+          });
           return <Event key={item.id} {...item} />;
         })}
       </div>
