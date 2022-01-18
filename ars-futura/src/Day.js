4b41fff8f31c06da0e5a4e30cfb896ebdb9b4ff0
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-18 18:00:30 +0100

    Sorting events  based on day and start time of the event

--- ars-futura/src/Day.js
+++ ars-futura/src/Day.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Event from "./Event";
+import { useAPIContext } from "./APIContext";
 
 const Day = ({ value }) => {
+  const { calendarEvents } = useAPIContext();
+
   return (
-    <div>
+    <div className="event-height">
       <h2>{value}</h2>
+      {calendarEvents.map((item) => {
+        /* renders number of event components for each present day, sorts array based on day and start time of the event */
+        calendarEvents.sort((a, b) => {
+          if (a.day === b.day) {
+            return a.startTimeNumber - b.startTimeNumber;
+          } else {
+            return a.day - b.day;
+          }
+        });
+        if (item.day === value) return <Event key={item.id} {...item} />;
+        return null;
+      })}
     </div>
   );
 };
