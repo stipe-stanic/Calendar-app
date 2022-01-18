c58fd47dcd802a6ffaebafb75900101d6c7de914
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-18 17:58:55 +0100

    Added show week and show today functionality
    Configured conditional rendering based on property values

--- ars-futura/src/DayList.js
+++ ars-futura/src/DayList.js
@@ -3,14 +3,55 @@ import { useAPIContext } from "./APIContext";
 import Day from "./Day";
 
 const DayList = () => {
-  const { days } = useAPIContext();
+  const {
+    days,
+    currentDate,
+    setShowToday,
+    showToday,
+    // setShowMonth,
+    // showMonth,
+  } = useAPIContext();
+
+  const toggleTodayView = () => {
+    setShowToday(!showToday);
+  };
+
+  /* display functionality for the events in next 30 days*/
+
+  // const toggleMonthView = () => {
+  //   setShowMonth(!showMonth);
+  // };
 
   return (
-    <section>
-      <div>Days List</div>
+    <section className="event-width">
+      <h2>Days List</h2>
+      <button onClick={toggleTodayView}>
+        {showToday && "Show week"}
+        {!showToday && "Show today"}
+      </button>
+      <hr />
+      {/* <button onClick={toggleMonthView}>Show month</button> */}
       <div>
         {days.map((item) => {
-          return <Day key={item.id} {...item} />;
+          /* renders all events in next seven days */
+          if (!showToday) {
+            if (item.value > currentDate.sevenDaysFromToday) {
+              return null;
+            }
+            return (
+              <div key={item.id}>
+                <Day {...item} />
+                <hr />
+              </div>
+            );
+            /* renders all events in the current day */
+          } else if (showToday) {
+            if (item.value === currentDate.today) {
+              return <Day key={item.id} {...item} />;
+            }
+            return null;
+          }
+          return null;
         })}
       </div>
     </section>
