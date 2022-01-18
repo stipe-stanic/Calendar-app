0bbfdfa84aac34ab961ccbdbe5188430ecacf861
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-18 17:56:35 +0100

    Commented out component for possible future use

--- ars-futura/src/EventList.js
+++ ars-futura/src/EventList.js
@@ -1,22 +1,28 @@
-import React from "react";
-import { useAPIContext } from "./APIContext";
-import Event from "./Event";
+/* Display events in order */
 
-export default function EventList() {
-  const { calendarEvents } = useAPIContext();
+// import React from "react";
+// import { useAPIContext } from "./APIContext";
+// import Event from "./Event";
 
-  return (
-    <section className="event-width">
-      <h2>Events</h2>
-      <hr />
-      <div>
-        {calendarEvents.map((item) => {
-          calendarEvents.sort((a, b) => {
-            return a.day - b.day;
-          });
-          return <Event key={item.id} {...item} />;
-        })}
-      </div>
-    </section>
-  );
-}
+// export default function EventList() {
+//   const { calendarEvents } = useAPIContext();
+
+//   return (
+//     <section className="event-width">
+//       <h2>Events</h2>
+//       <hr />
+//       <div>
+//         {calendarEvents.map((item) => {
+//           calendarEvents.sort((a, b) => {
+//             if (a.day === b.day) {
+//               return a.startTimeNumber - b.startTimeNumber;
+//             } else {
+//               return a.day - b.day;
+//             }
+//           });
+//           return <Event key={item.id} {...item} />;
+//         })}
+//       </div>
+//     </section>
+//   );
+// }
