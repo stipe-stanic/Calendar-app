1096329741fb3a968eb2ec2dd1dc94a6c2e587db
Stipe Stanic <stipe.stanic156@gmail.com>
2022-01-25 21:47:32 +0100

    Added GlobalProvider

--- ars-futura/src/index.js
+++ ars-futura/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { APIProvider } from "./APIContext";
+import { APIProvider } from "./Context/APIContext";
 import "./index.css";
 import App from "./App";
+import { GlobalProvider } from "./Context/GlobalContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <APIProvider>
-      <App />
-    </APIProvider>
+    <GlobalProvider>
+      <APIProvider>
+        <App />
+      </APIProvider>
+    </GlobalProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
